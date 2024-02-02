3ab034e2798a667ab7ae4b4fa486263bbea353b5
amandaspavan <amandaspavan@gmail.com>
2024-02-02 16:54:47 +0100

    exercício-1

--- orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
+++ orientacao-a-objetos/2-classes-e-objetos/lista-de-exercicios-1.py
@@ -1,13 +1,29 @@
 # Crie uma classe que modele o objeto "carro".
 # Um carro tem os seguintes atributos: ligado, cor, modelo, velocidade.
 # Um carro tem os seguintes comportamentos: liga, desliga, acelera, desacelera.
-
-
+class Carro:
+    def __init__(self):
+        self.cor = "vermelho"
+        self.ligado = False
+    def ligar(self):
+        self.ligado = True
+    def desligar(self):
+        self.ligado = False
+    def acelerar(self):
+        self.velocidade = True
+    def desacelerar(self):
+        self.velocidade = False
 
 # Crie uma instância da classe carro.
-
+meu_carro = Carro()
 
 # Faça o carro "andar" utilizando os métodos da sua classe.
-
+meu_carro.ligar()
+meu_carro.acelerar()
 
 # Faça o carro "parar" utilizando os métodos da sua classe.
+meu_carro.desacelerar()
+meu_carro.desligar()
+
+print('meu_carro está ligado? {}'.format(meu_carro.ligado))
+print('meu_carro está acelerando? {}'.format(meu_carro.velocidade))
